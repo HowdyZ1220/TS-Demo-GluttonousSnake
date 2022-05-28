e65113c6a019fc6d1fbde3464cdfc7433c69511b
wonidie <982796687@qq.com>
2022-05-28 17:20:30 +0800

    完成food类

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,32 @@
 import "./style/style.less";
-console.log(111);
+
+//定义食物类Food
+class Food {
+  element: HTMLElement;
+
+  constructor() {
+    this.element = document.querySelector("#food")!;
+  }
+  get X() {
+    return this.element.offsetLeft;
+  }
+  get Y() {
+    return this.element.offsetHeight;
+  }
+  change() {
+    const left = Math.floor(Math.random() * 29) * 10;
+    const top = Math.floor(Math.random() * 29) * 10;
+    console.log(this.element.style);
+
+    this.element.style.left = left + "px";
+    this.element.style.top = top + "px";
+  }
+}
+
+const food = new Food();
+
+console.log(food.X);
+console.log(food.Y);
+food.change();
+console.log(food.X);
+console.log(food.Y);
